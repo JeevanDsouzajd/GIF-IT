58f4b846ab7842995de478a258ea96d1b2ac37f9
Jeevan Dsouza <Jdjeevan26@gmail.com>
2022-07-29 12:26:40 +0530

    final commit

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            <i class="fa-solid fa-spinner fa-4x fa-spin"></i>
+        </div>
+    )
+}
+
+export default Loader;
